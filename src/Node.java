95bbda39c62b041bdc4a297bf2ccaf2934de1b6b
ivescai <caij1@rose-hulman.edu>
2020-02-14 18:23:11 -0500

    slightly change in node

--- src/Node.java
+++ src/Node.java
@@ -3,10 +3,18 @@ import java.util.HashMap;
 
 public class Node {
 
-
+	private String name;
+	private Integer x;
+	private Integer y;
+	
 	private HashMap<Node, Path> paths;
 	private ArrayList<Node> nodes;
 
+	public Node(String name, int x, int y) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
 	
 
 }
