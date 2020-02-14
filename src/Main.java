2dde94fc47f68c1ec1c8c9b0ef8d19af8de9b9c3
Cullen LaKemper <lakempca@rose-hulman.edu>
2020-02-14 14:19:19 -0500

    Worked on the D man algoryth

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-
+	
+	Graph graph;
+	WindowFrame windowFrame;
+	
 	public static void main(String[] args) {
 
 	}
--- src/Path.java
+++ src/Path.java
@@ -1,4 +1,11 @@
-
 public class Path {
 
+	public enum Mode {WALKING, HORSE, BOAT};
+	int distance;
+	
+	
+	
+	
+	
+	
 }
