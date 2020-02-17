fec8fd930752c7f6f0b4ea8da30904734593776e
ivescai <caij1@rose-hulman.edu>
2020-02-16 21:25:08 -0500

    add methods I have so far to Node and Path

--- src/Path.java
+++ src/Path.java
@@ -1,8 +1,36 @@
-import java.util.ArrayList;
 
 public class Path {
+	
+	Integer distance;
+	Node n1;
+	Node n2;
+	
+	public Path(Node n1, Node n2) {
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	public void setDist(int dist) {
+		this.distance = dist;
+	}
+	
+	public Integer getDist() {
+		return this.distance;
+	}
+	
+	public Node[] getBothNode() {
+		Node[] ns = {n1, n2};
+		return ns;
+	}
+	
+	public Node getOtherNode(Node n) {
+		if (n.equals(this.n1)) {
+			return this.n2;
+		} else if (n.equals(this.n2)) {
+			return this.n1;
+		}
+		return null;
+	}
 
-	int distance;
-	ArrayList<Graph.Mode> modes;
-
+	
 }
